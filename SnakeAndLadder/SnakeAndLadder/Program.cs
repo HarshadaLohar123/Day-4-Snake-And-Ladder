968e120b7d48534b4da2f3b9bec4c05b515b41f0
harshadalohar123 <harshalohar123@gmail.com>
2022-03-15 12:46:54 +0530

    [Harshada]: Played with single player at start position 0

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    internal class SnakeAndLadder
+    {
+        public static void Uc_1_SinglePlayer()
+        {
+            Console.WriteLine("Welcome To Snake And Ladde Problem");
+            Console.WriteLine("Snake and Ladder game played with single player start at zaro");
+        }
+        static void Main(string[] args)
+        {
+            SnakeAndLadder.Uc_1_SinglePlayer();
+        }
+    }
+}
