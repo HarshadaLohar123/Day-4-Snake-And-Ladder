252da72cca7a9db5a680417493390edbc158eaab
harshadalohar123 <harshalohar123@gmail.com>
2022-03-15 12:53:11 +0530

    [Harshada]: Player roll the die

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -9,9 +9,18 @@ namespace SnakeAndLadder
             Console.WriteLine("Welcome To Snake And Ladde Problem");
             Console.WriteLine("Snake and Ladder game played with single player start at zaro");
         }
+        public static void UC_2_PlayerRollDie()
+        {
+            Console.WriteLine("Welcome to UC-2 program");
+            Random rnd = new Random();
+            int num = rnd.Next(1, 7);
+            Console.WriteLine("The Random Dice Number is: " + num);
+        
+        }
         static void Main(string[] args)
         {
             SnakeAndLadder.Uc_1_SinglePlayer();
+            SnakeAndLadder.UC_2_PlayerRollDie();
         }
     }
 }
