53d4e245910e22093838f2ba839fa65a62b2584b
harshadalohar123 <harshalohar123@gmail.com>
2022-03-15 13:15:23 +0530

    [Harshada]: Player reaches the winning position 100

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -48,11 +48,61 @@ namespace SnakeAndLadder
                     }
             }
         }
+        public static void UC_4_PlayerReachesWinningPosition100()
+        {
+            int ladder = 1, ldice;
+            int snake = 2, sdice;
+            int win = 100;
+            int count = 0;
+            Console.WriteLine("Welcome to UC_4");
+            while (count <= win)
+            {
+                if (count >= 0)
+                {
+                    Random random = new Random();
+                    int randomCheck = random.Next(3);
+
+                    switch (randomCheck)
+                    {
+                        case 1:
+                            {
+                                Random rnd = new Random();
+                                ldice = rnd.Next(1, 7);
+                                count = count + ldice;
+                                Console.WriteLine("Got Ladder");
+                                break;
+                            }
+                        case 2:
+                            {
+                                Random sran = new Random();
+                                sdice = sran.Next(1, 7);
+                                count = count - sdice;
+                                Console.WriteLine("Got snake");
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("No play");
+                                break;
+                            }
+                    }
+                }
+                else
+                {
+                    count = 0;
+                }
+
+            }
+            Console.WriteLine("Player wins" + count);
+        }
+
+
         static void Main(string[] args)
         {
             SnakeAndLadder.Uc_1_SinglePlayer();
             SnakeAndLadder.UC_2_PlayerRollDie();
             SnakeAndLadder.UC_3_PlayGame();
+            SnakeAndLadder.UC_4_PlayerReachesWinningPosition100();
         }
     }
 }
