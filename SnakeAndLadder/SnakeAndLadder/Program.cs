773610f12825a0009bb12f8c5efcbcac59ec002d
harshadalohar123 <harshalohar123@gmail.com>
2022-03-15 13:03:18 +0530

    [Harshada]: Check Ladder Or Snake

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -15,12 +15,45 @@ namespace SnakeAndLadder
             Random rnd = new Random();
             int num = rnd.Next(1, 7);
             Console.WriteLine("The Random Dice Number is: " + num);
-        
+
+        }
+        public static void UC_3_PlayGame()
+        {
+            int ladder = 1, ldice;
+            int snake = 2, sdice;
+            Console.WriteLine("Welcome to UC_3");
+            Random random = new Random();
+            int randomCheck = random.Next(3);
+
+            switch (randomCheck)
+            {
+                case 1:
+                    {
+                        Random rnd = new Random();
+                        ldice = rnd.Next(1, 7);
+                        Console.WriteLine("Got Ladder");
+                        break;
+                    }
+                case 2:
+                    {
+                        Random sran = new Random();
+                        sdice = sran.Next(1, 7);
+                        Console.WriteLine("Got snake");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("No play, Stay at base");
+                        break;
+                    }
+            }
         }
         static void Main(string[] args)
         {
             SnakeAndLadder.Uc_1_SinglePlayer();
             SnakeAndLadder.UC_2_PlayerRollDie();
+            SnakeAndLadder.UC_3_PlayGame();
         }
     }
 }
+
